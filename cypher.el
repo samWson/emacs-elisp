ff5fdd52731b43cdb9dd873ce8f33244f6dac3a7
samWson <samWson@users.noreply.github.com>
2017-06-05 21:08:03 +1200

    Add constants and alphabet function.

--- cypher.el
+++ cypher.el
@@ -3,6 +3,9 @@
 ;; Single characters can return their ascii value by preceding them
 ;; with '?' e.g. ?A -> 65
 
+(defconst ascii-A ?A "The ascii value of 'A'")
+(defconst ascii-Z ?Z "The ascii value of 'Z'")
+
 (defun inc (num)
   "Return num incremented by 1"
   (+ num 1))
@@ -15,9 +18,12 @@ If start is greater than end '() is returned."
       '()
     (cons start (range (inc start) end))))
 
-(let ((nums (range ?A ?Z)))
-  (mapcar 'byte-to-string nums))
-("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" ...)
+(defun alphabet ()
+  "Return an ordered alphabet from start to end
+
+Uses the global constants ascii-A and ascii-Z."
+  (let ((nums (range ascii-A ascii-Z)))
+    (mapcar 'byte-to-string nums)))
 
 ;; I can make the list of numbers, and convert them to the
 ;; alphabet. Now figure out how to create an infinitely cycling lazy sequence.
