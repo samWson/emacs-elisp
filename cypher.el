7851d841e58f8b88e8e04f0efef298d9e50c071d
Samuel Wilson <samWson@users.noreply.github.com>
2017-06-12 07:22:06 +1200

    Remove technical debt

--- cypher.el
+++ cypher.el
@@ -6,20 +6,6 @@
 (defconst ascii-A ?A "The ascii value of 'A'")
 (defconst ascii-Z ?Z "The ascii value of 'Z'")
 
-;;; Technical debt - redundent function, use built in 1+
-;; (defun inc (num)
-;;   "Return num incremented by 1"
-;;   (+ num 1))
-
-;;; Technical debt - redundent function, use built in number-sequence
-;; (defun range (start end)
-;;   "Return a list of integers from start to end
-
-;; If start is greater than end '() is returned."
-;;   (if (> start end)
-;;       '()
-;;     (cons start (range (inc start) end))))
-
 (defun alphabet ()
   "Return an ordered alphabet from start to end
 
